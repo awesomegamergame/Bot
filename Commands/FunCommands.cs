c06c4ddc6254717691c13d69e6fa9fa9f2cf0c1b
Awesomegamergame <awesomegamergames001@gmail.com>
2022-01-13 20:38:55 -0500

    Upload bot code will make public if config.json doesnt get uploaded

---
+++ Commands/FunCommands.cs
@@ -0,0 +1,27 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Threading.Tasks;
+
+namespace Discord_Bot.Commands
+{
+    public class FunCommands : BaseCommandModule
+    {
+        [Command("ping")]
+        [Description("Returns Pong")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Pong").ConfigureAwait(false);
+        }
+
+        [Command("add")]
+        [Description("Adds two numbers together")]
+        public async Task Add(CommandContext ctx,
+            [Description("First Number")] int numberOne,
+            [Description("Second Number")] int numberTwo)
+        {
+            await ctx.Channel
+                .SendMessageAsync((numberOne + numberTwo).ToString())
+                .ConfigureAwait(false);
+        }
+    }
+}
---
+++ ConfigJson.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot
+{
+    class ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+        [JsonProperty("prefix")]
+        public string Prefix { get; private set; }
+    }
+}
